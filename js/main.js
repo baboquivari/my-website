985e611f17dbb60e37bcbce636e53f57138d0940
Tony Sian <to.sian.san@gmail.com>
2017-01-26 17:22:17 +0000

    Making modifications

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
 $(document).ready(function () {
   $("h1").click(function () {
   	console.log("clicked");			
- });
+ 	});
+});
 
-  $("1").blur();
+$(document).ready(function() {
+    $(".col-sm-6 img").click(function() {
+        $(this).css("opacity", "1");
+    	});
+    		
+
+
+
 
 
 
 });
+// MAKE THOSE IMAGE FULL WHEN YOU CLICK, USING "THIS"
+
+
+
+
+
+
+// .col-sm-6 img {
+//  opacity: 0.017;
+//}
